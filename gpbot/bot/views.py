b5c0bfa313b3f1be2b38ec9b86d814345dd961ef
KAGE <kagemiku@gmail.com>
2017-02-17 23:46:49 +0900

    Add reply function

--- gpbot/bot/views.py
+++ gpbot/bot/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
+import json
+import requests
 
-# Create your views here.
+REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
+CHANNEL_ACCESS_TOKEN = os.environ['LINE_CHANNEL_ACCESS_TOKEN']
+HEADER = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + CHANNEL_ACCESS_TOKEN
+}
+
+def reply_text(reply_token, text):
+    reply = text
+    payload = {
+        'replyToken': reply_token,
+        'message': [
+            {
+                'type': 'text',
+                'text': reply,
+            },
+        ]
+    }
+
+    requests.post(REPLY_ENDPOINT, headers=HEADER, data=json.dumps(payload))
+    return reply
+
+def callback(request):
+    reply = ''
+    print('requiest: ', request)
+    request_json = json.loads(request.body.decode('utf-8'))
+    for e in request_json['events']:
+        reply_token = e['replyToken']
+        message_type = e['message']['type']
+
+        if message_type == 'text':
+            text = e['message']['text']
+            reply += reply_text(reply_token, text)
+
+    return HttpResponse(reply)
+
